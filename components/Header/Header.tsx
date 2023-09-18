8ce916cbda6095289d321d42555b35d2484b8bf1
zuzannanap <105361496+zuzannanap@users.noreply.github.com>
2023-09-18 16:45:58 +0200

    fix home link for logo + app name

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,20 +7,19 @@ import { navigationItems } from "../Navigation/utils";
 export const Header = () => {
   return (
     <div className='flex w-full h-fit text-green-400 px-8 py-4 items-center gap-2'>
-      <Link
-        className='rounded-full bg-violetDark p-2 flex items-center h-fit min-w-fit justify-center'
-        href='/'
-        aria-label='Go to homepage'>
-        <Image
-          src='/logo.png'
-          alt='logo'
-          sizes='100vw'
-          width='0'
-          height='0'
-          className='w-10 h-10 pt-1'
-        />
+      <Link href='/' aria-label='Go to homepage' className='flex items-center justify-center gap-2'>
+        <div className='rounded-full bg-violetDark p-2 flex items-center h-fit min-w-fit justify-center'>
+          <Image
+            src='/logo.png'
+            alt='logo'
+            sizes='100vw'
+            width='0'
+            height='0'
+            className='w-10 h-10 pt-1'
+          />
+        </div>
+        <span className='font-bold text-violetDark text-lg'>FitoFit</span>
       </Link>
-      <span className='font-bold text-violetDark text-lg'>FitoFit</span>
       <Navigation items={navigationItems} />
     </div>
   );
