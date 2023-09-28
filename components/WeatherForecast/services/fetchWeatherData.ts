a38fedadd13974accf5e9db1b3fdbe24e87fb0b6
zuzannanap <105361496+zuzannanap@users.noreply.github.com>
2023-09-28 22:06:59 +0200

    add OpenWeatherMap env

--- components/WeatherForecast/services/fetchWeatherData.ts
+++ components/WeatherForecast/services/fetchWeatherData.ts
@@ -3,11 +3,11 @@ import { OpenWeatherMapData } from "../DTO/OpenWeatherMapData.interface";
 import { Coordinates } from "../Weather.interface";
 
 export async function fetchWeatherData({ lat, lon }: Coordinates): Promise<OpenWeatherMapData> {
-  // if (!process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY) {
-  //   throw new Error("No OpenWeatherMap API key");
-  // }
-  // const apiKey = process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY;
-  const apiKey = "482a3ff2b48be084f28cff9a6d876094";
+  //env file was not added to .gitignore because:
+  // 1. this (the only one in the project) env is exposed as part of the request url anyway
+  // 2. I wanted the app to work properly after deploying it on github
+
+  const apiKey = process.env.NEXT_PUBLIC_OPEN_WEATHER_MAP_API_KEY;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
   try {
     const response = await axios.get(url);
